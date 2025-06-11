52afa96449ee5d347ae5ae0ed9260ffe37e96d1d
Akshay Pandit <akshaycpandit19@gmail.com>
2025-06-11 19:34:27 +0530

    Add unique secret key in jwt config (same as .env)

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('jwt', () => ({
-  secret: process.env.JWT_SECRET || 'your-secret-key',
+  secret: process.env.JWT_SECRET || 'myS3cr3tK3y!@062025$%^&*()_+aBcDeFgHiJkLmNoP',
   expiresIn: process.env.JWT_EXPIRATION || '1d',
 })); 
