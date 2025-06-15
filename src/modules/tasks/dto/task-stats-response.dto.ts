f0b9e4dd9e9fc4ef5e441ec5a0465e8621dc704f
Akshay Pandit <akshaycpandit19@gmail.com>
2025-06-15 19:02:24 +0530

    tasks stats response dto created for structured response on getStats API endpoint

---
+++ src/modules/tasks/dto/task-stats-response.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { TaskStatus } from '../enums/task-status.enum';
+import { TaskPriority } from '../enums/task-priority.enum';
+
+export class TaskStatsResponseDto {
+  @ApiProperty({ example: 10 })
+  total: number;
+
+  @ApiProperty({ example: 4 })
+  completed: number;
+
+  @ApiProperty({ example: 3 })
+  inProgress: number;
+
+  @ApiProperty({ example: 2 })
+  pending: number;
+
+  @ApiProperty({ example: 1 })
+  highPriority: number;
+} 
