8f2ca6ef7f23b17be3e24c978add5b905b7092cf
Akshay Pandit <akshaycpandit19@gmail.com>
2025-06-17 04:18:42 +0530

    Setup CQRS pre-requisites in tasksModule

--- src/modules/tasks/tasks.module.ts
+++ src/modules/tasks/tasks.module.ts
@@ -4,9 +4,22 @@ import { BullModule } from '@nestjs/bullmq';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { Task } from './entities/task.entity';
+import { CommandHandler, CqrsModule } from '@nestjs/cqrs';
+import { CreateTaskHandler } from './commands/handlers/create-task.handler';
+import { GetTasksHandler } from './queries/handlers/get-tasks.handler';
 
+const commandHandlers = [
+  CreateTaskHandler,
+  // Add others like UpdateTaskHandler, DeleteTaskHandler, BatchProcessHandler
+];
+
+const queryHandlers = [
+  GetTasksHandler,
+  // Add others like GetTaskByIdHandler, GetStatsHandler
+];
 @Module({
   imports: [
+    CqrsModule,
     TypeOrmModule.forFeature([Task]),
     BullModule.registerQueue({
       name: 'task-processing',
@@ -16,7 +29,11 @@ import { Task } from './entities/task.entity';
     }),
   ],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [
+    TasksService,
+    ...commandHandlers,
+    ...queryHandlers,
+  ],
   exports: [TasksService],
 })
 export class TasksModule {} 
