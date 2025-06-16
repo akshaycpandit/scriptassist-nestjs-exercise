43e42c5860c4ddc48d1098d5d3bba56375cba682
Akshay Pandit <akshaycpandit19@gmail.com>
2025-06-17 05:11:58 +0530

    createTasks moved from controller to command handler

--- src/modules/tasks/commands/handlers/create-task.handler.ts
+++ src/modules/tasks/commands/handlers/create-task.handler.ts
@@ -1,12 +1,16 @@
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { TasksService } from '../../tasks.service';
 import { CreateTaskCommand } from '../impl/create-task.command';
+import { TaskResponseDto } from '../../dto/task-response.dto';
+import { plainToInstance } from 'class-transformer';
 
 @CommandHandler(CreateTaskCommand)
 export class CreateTaskHandler implements ICommandHandler<CreateTaskCommand> {
   constructor(private readonly tasksService: TasksService) {}
 
   async execute(command: CreateTaskCommand) {
-    return this.tasksService.create(command.createTaskDto);
+    const { createTaskDto } = command;
+    const createdTask = await this.tasksService.create(createTaskDto);
+    return plainToInstance(TaskResponseDto, createdTask);
   }
 }
